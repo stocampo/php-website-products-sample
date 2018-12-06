35bff3b47058cc845c50aca7fd0697ee24d849d4
Stephen Ocampo <41347940+stocampo@users.noreply.github.com>
2018-12-05 19:16:34 -0500

    Add files via upload

---
+++ stephen ocampo php site/inc/data.php
@@ -0,0 +1,161 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "It: A Novel (reissue)",
+	"img" => "img/media/it.jpg",
+ "genre" => "Horror",
+ "format" => "Paperback",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+	 "Stephen King"
+    ],
+		"publisher" => "Scribner",
+	  "isbn" => '9781501142970'
+];
+$catalog[102] = [
+	"title" => "Pet Sematary",
+	"img" => "img/media/petsem.jpg",
+ "genre" => "Horror",
+ "format" => "Hardcover",
+ "year" => 2002,
+ "category" => "Books",
+ "authors" => [
+	 "Stephen King"
+    ],
+		"publisher" => "Gallery Books",
+	  "isbn" => '9780743412285'
+];
+$catalog[103] = [
+	"title" => "The Mist",
+	"img" => "img/media/mist.jpg",
+ "genre" => "Horror",
+ "format" => "Paperback",
+ "year" => 2018,
+ "category" => "Books",
+ "authors" => [
+	 "Stephen King"
+    ],
+		"publisher" => "Scribner",
+	  "isbn" => '	9781982103521'
+];
+$catalog[104] = [
+	"title" => "Cujo: A Novel",
+	"img" => "img/media/cujo.jpg",
+ "genre" => "Horror",
+ "format" => "Hardover",
+ "year" => 2016,
+ "category" => "Books",
+ "authors" => [
+	 "Stephen King"
+    ],
+		"publisher" => "Gallery Books",
+	  "isbn" => '9781501192241'
+];
+//Movies
+$catalog[201] = [
+	"title" => "The Godfather Collection",
+  "img" => "img/media/godfather.jpg",
+  "genre" => "Drama",
+  "format" => "DVD",
+  "year" => 2008,
+  "category" => "Movies",
+  "director" => "Francis Ford Coppola",
+  "writers" => [
+      "Francis Ford Coppolan",
+   "Mario Puzo"
+    ],
+		"stars" => [
+	      "Al Pacino",
+	      "Robert Duvall",
+	      "Diane Keaton"
+    ]
+];
+$catalog[202] = [
+	"title" => "Rocky: Heavyweight Collection",
+  "img" => "img/media/Rocky.jpg",
+  "genre" => "Action,Drama",
+  "format" => "DVD",
+  "year" => 2014,
+  "category" => "Movies",
+  "director" => "John G. Avildsen",
+  "writers" => [
+		"Sylvester Stallone"
+    ],
+		"stars" => [
+			"Sylvester Stallone"
+    ]
+];
+$catalog[203] = [
+	"title" => "Die Hard Collection",
+  "img" => "img/media/diehard.jpg",
+  "genre" => "Action",
+  "format" => "DVD",
+  "year" => 2018,
+  "category" => "Movies",
+  "director" => "Renny Harlin",
+  "writers" => [
+      "Doug Richardson",
+      "Steven E. de Souza"
+    ],
+		"stars" => [
+	      "Bruce Willis",
+	      "Bonnie Bedelia "
+    ]
+];
+$catalog[204] = [
+	"title" => "The Expendables: 3-Film Collection",
+	"img" => "img/media/expend.jpg",
+	"genre" => "Action,",
+	"format" => "DVD",
+	"year" => 2017,
+	"category" => "Movies",
+	"director" => "Sylvester Stallone",
+	"writers" => [
+			"Sylvester Stallone",
+			"Dave Callaham"
+    ],
+		"stars" => [
+	      "Sylvester Stallone",
+	      "Jason Statham",
+	      "Mickey Rourke"
+    ]
+];
+//Music
+$catalog[301] = [
+	"title" => "Road to Ruin (reissue)",
+  "img" => "img/media/ramones.jpg",
+  "genre" => "Rock",
+  "format" => "Vinyl",
+  "year" => 2018,
+  "category" => "Music",
+  "artist" => "Ramones"
+];
+$catalog[302] = [
+	"title" => "Greatest Hits",
+  "img" => "img/media/queen.jpg",
+  "genre" => "Rock",
+  "format" => "CD, Vinyl",
+  "year" => 2004,
+  "category" => "Music",
+  "artist" => "Queen"
+];
+$catalog[303] = [
+	"title" => "Thriller (reissue)",
+  "img" => "img/media/jackson.jpg",
+  "genre" => "Pop",
+  "format" => "Vinyl",
+  "year" => 2015,
+  "category" => "Music",
+  "artist" => "Michael Jackson"
+];
+$catalog[304] = [
+	"title" => "Abbey Road (reissue)",
+  "img" => "img/media/beatles.jpg",
+  "genre" => "Rock, Pop",
+  "format" => "Vinyl",
+  "year" => 2012,
+  "category" => "Music",
+  "artist" => "The beatles"
+];
